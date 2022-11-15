5c89b09c77f553a33c1885fd51f0c9aca5e9eb03
Nazariy <nazkamaz@gmail.com>
2022-11-15 11:43:48 +0200

    pritty

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,8 +13,8 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
+    // name: '',
+    // number: '',
     filter: '',
   };
 
